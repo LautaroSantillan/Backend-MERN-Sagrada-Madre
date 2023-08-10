1059c329171658fd7356359701381be8b44797fc
LautaroSantillan <santillanlautaro03@gmail.com>
2023-08-10 09:54:46 -0300

    Created login and logout -v5

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { register, login } from "../controllers/auth.controller.js";
+import { register, login, logout } from "../controllers/auth.controller.js";
 
 const router = Router();
 
+// ROUTES POST
 router.post('/register', register);
 router.post('/login', login);
-
+router.post('/logout', logout)
 
 export default router;
